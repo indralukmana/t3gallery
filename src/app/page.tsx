c16cdeee95f895b9cf003648604b08f75125360f
indralukmana <indralukmana@live.com>
2024-06-13 20:26:43 +0700

    feat: :passport_control: setup authorization on main gallery page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,36 @@
-import Link from "next/link";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function GalleryImages() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
-
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => {
-          return (
-            <div key={image.id} className="w-48">
-              <img src={image.url} alt={image.name ?? "image"} />
-            </div>
-          );
-        })}
-      </div>
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => {
+        return (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt={image.name ?? "image"} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main className="h-full w-full">
+      <SignedOut>
+        <div className="grid h-full w-full place-items-center">
+          <p>Please sign in to show your gallery</p>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <GalleryImages />
+      </SignedIn>
     </main>
   );
 }
